d07a5ac05c39ccf51929bca14a279ac524049bf0
ShadowNinja909 <Terrell.AlexisWilson@edu.sait.ca>
2024-12-01 18:34:23 -0700

    Create page for testing search function
    - This should be deleted before pulling with main

---
+++ app/test/page.js
@@ -0,0 +1,11 @@
+import react from "react";
+import SearchForm from "../components/SearchForm";
+
+export default function TestPage() {
+    return (
+        <div>
+            <h1>Test Page</h1>
+            <SearchForm />
+        </div>
+    )
+}
